292016984dcdfdec63a5ef6e07461a860f74872f
Jose Eduardo Sierra Vargas <jsierrav14@gmail.com>
2020-10-05 11:06:09 -0500

    loading url with cypress

--- cypress/integration/examples/simple.spec.js
+++ cypress/integration/examples/simple.spec.js
@@ -1,5 +1,20 @@
 describe('My firsts simple test with cypress', () => {
     it('True should be true', () => {
-       expect(true).to.equal(false)
+       expect(true).to.equal(true)
+    })
+
+    it('5 should be 5',()=>{
+        expect(5).to.equal(5)
+    })
+})
+describe('Another describe block',()=>{
+    it('false should be false',()=>{
+        expect(false).to.equal(false)
+    })
+})
+
+describe('Browser Actions', ()=>{
+    it('Should load correct url',()=>{
+        cy.visit('https://reactjs.org/',{timeout:10000})
     })
 })
